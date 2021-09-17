875833dee81d3287b7c8f8307600e51c1c265675
Kirill <freshjs.ftd@gmail.com>
2021-09-17 14:36:12 +0300

    Add Image jsx example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import Image from './components/Image';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Image alt="phone image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png" />
 }
 
 export default App;
---
+++ src/components/Image/index.jsx
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+
+class Image extends Component {
+
+  render() {
+    const {src, alt} = this.props;
+    
+    return <div className="imgWrapper">
+      <img className="img" src={src} alt={alt}/>
+    </div>
+  }
+}
+
+export default Image;
