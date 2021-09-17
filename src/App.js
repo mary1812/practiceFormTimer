2be4e2bfe1a33f5ae9a54a10ff3b827b38ed8588
Kirill <freshjs.ftd@gmail.com>
2021-09-17 16:39:32 +0300

    Add Clicker solution

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Aloha from './components/Aloha';
-import Image from './components/Image';
+import Clicker from './components/Clicker';
+
 
 function App() {
   return (
     <React.Fragment>
       <Aloha name="Andrey" isGreeting />
       <Aloha name="Vika"/>
-      <Image
-        alt="img"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png"
-      />
+      <Clicker/>
     </React.Fragment>
   );
 }
--- src/components/Clicker/index.jsx
+++ src/components/Clicker/index.jsx
@@ -1,7 +1,42 @@
 /* 
   Создайте компонент Clicker
   у него в состоянии должен быть счетчик количества кликов по нему
-  Компонент должен выводить на экран количестов кликов и кнопку, 
-  по нажатию на которую счетчик растет
+  Компонент должен выводить на экран количестов кликов 
+  и кнопку, по нажатию на которую счетчик растет
 
 */
+
+import { Component } from 'react';
+
+class Clicker extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clickAmount : 0
+    }
+  }
+
+  handleClick = () => {
+    const { clickAmount } = this.state;
+
+    this.setState({
+      clickAmount : clickAmount + 1
+    });
+  }
+
+  render() {
+    console.log(this);
+    const {clickAmount} = this.state;
+
+
+    return (
+    <div>
+      <p>Нажали {clickAmount} раз</p>
+      <button onClick={this.handleClick}>жми</button>
+    </div>)
+  }
+}
+
+
+export default Clicker;
